5904ed835a70eba3104878703d9c59fa389f5929
wesleybriones <wesgbrio@espol.edu.ec>
2021-08-14 13:22:21 -0500

    componentes&icons

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
-
-  ngOnInit() {}
+  @Input() titulo:string = '';
 
 }
